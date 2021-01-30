0e6e062f7eb229a8a531a5f3b23065438153ab57
Guillaume Briant <guillaume.briant@free.fr>
2021-01-31 00:15:31 +0100

    Pick up cat

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake() {
+        current = this;
+    }
+
+    public event Action onPickUpButtonTrigger;
+    public void PickUp() {
+        if (onPickUpButtonTrigger != null) {
+            onPickUpButtonTrigger();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using Cinemachine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private Transform camera;
-
     [SerializeField] private float speed;
     [SerializeField] private float fallMultiplier;
     [SerializeField] private float jumpMultiplier;
     [SerializeField] private float jumpVelocity;
     [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
+    [SerializeField] private Transform catPos;
+    [SerializeField] private CinemachineFreeLook cinemachine;
+
+    [HideInInspector] public bool canInteract = false;
+    [HideInInspector] public string interactableObjectName;
+    [HideInInspector] public GameObject interactableObjectNear;
 
     private Rigidbody rb;
     private Vector2 dir;
     private Vector3 moveDir;
+    private bool isCatCarried;
+    private bool isInInteractMenu;
+    private float cinemachineYSpeed;
+    private float cinemachineXSpeed;
 
     [HideInInspector] public bool isGrounded = true;
 
+    #region MonoBehaviour
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        cinemachineYSpeed = cinemachine.m_YAxis.m_MaxSpeed;
+        cinemachineXSpeed = cinemachine.m_XAxis.m_MaxSpeed;
+
+        GameEvents.current.onPickUpButtonTrigger += PickUpCat;
     }
 
     private void Start() {
@@ -33,8 +49,7 @@ public class Player : MonoBehaviour
         Rotate();
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         Walk();
 
         if (rb.velocity.y < 0) {
@@ -45,12 +60,21 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void OnDestroy() {
+        GameEvents.current.onPickUpButtonTrigger -= PickUpCat;
+    }
+    #endregion
+
+    #region Methods
     private void Walk() {
-        rb.velocity = new Vector3(moveDir.x * speed, rb.velocity.y, moveDir.z * speed);
+        if (isInInteractMenu)
+            rb.velocity = Vector3.zero;
+        else
+            rb.velocity = new Vector3(moveDir.x * speed, rb.velocity.y, moveDir.z * speed);
     }
 
     public void Jump() {
-        if (isGrounded) {
+        if (isGrounded && !isInInteractMenu) {
             isGrounded = false;
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
             rb.velocity += Vector3.up * jumpVelocity;
@@ -58,10 +82,10 @@ public class Player : MonoBehaviour
     }
 
     private void Rotate() {
-        if (dir.magnitude >= 0.1f) {
+        if (dir.magnitude >= 0.1f && !isInInteractMenu) {
             float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            
+
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
@@ -70,4 +94,59 @@ public class Player : MonoBehaviour
             moveDir = Vector3.zero;
         }
     }
+
+    public void Interact() {
+        if (canInteract) {
+            switch (interactableObjectName) {
+                case ("Cat"):
+                    if (isInInteractMenu) {
+                        UiManager.current.HideCatInteractionCanvas();
+                        isInInteractMenu = false;
+                        ResumeCinemachine();
+                    }
+                    else {
+                        UiManager.current.ShowCatInteractionCanvas();
+                        isInInteractMenu = true;
+                        StopCinemachine();
+                    }
+
+                    if (isCatCarried) UiManager.current.DisablePickUpButton();
+                    
+                    break;
+
+                case ("Quest"):
+                    isInInteractMenu = true;
+                    StopCinemachine();
+                    break;
+
+                case ("House"):
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void PickUpCat() {
+        UiManager.current.HideCatInteractionCanvas();
+        isInInteractMenu = false;
+        ResumeCinemachine();
+
+        isCatCarried = true;
+        interactableObjectNear.transform.SetParent(catPos);
+        interactableObjectNear.transform.DOLocalMove(Vector3.zero, 1f);
+        interactableObjectNear.transform.DOLocalRotate(Vector3.zero, 1f);
+    }
+
+    private void StopCinemachine() {
+        cinemachine.m_YAxis.m_MaxSpeed = 0f;
+        cinemachine.m_XAxis.m_MaxSpeed = 0f;
+    }
+
+    private void ResumeCinemachine() {
+        cinemachine.m_YAxis.m_MaxSpeed = cinemachineYSpeed;
+        cinemachine.m_XAxis.m_MaxSpeed = cinemachineXSpeed;
+    }
+    #endregion
 }
