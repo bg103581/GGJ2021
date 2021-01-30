fa753738f63d965bf38bcfa6ef05897b644771e0
Guillaume Briant <guillaume.briant@free.fr>
2021-01-30 12:50:07 +0100

    walk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [SerializeField] private float speed;
+
+    private Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
+    }
+
+    private void Walk() {
+        Vector2 dir = InputManager.current.direction;
+        rb.velocity = new Vector3(dir.x, 0, dir.y).normalized * speed * Time.deltaTime;
     }
 }
