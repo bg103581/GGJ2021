78dcffa0cc4d94481c524c3aab152e9797a25d01
Guillaume Briant <guillaume.briant@free.fr>
2021-01-31 13:53:58 +0100

    set up score

---
+++ Assets/Scripts/ReputationBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReputationBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    public void SetMaxReputation(int reput) {
+        slider.maxValue = reput;
+        slider.value = 0;
+    }
+
+    public void SetReputation(int reput) {
+        slider.value = reput;
+    }
+
+    public void AddReputation(int reput) {
+        int sum = (int)slider.value + reput;
+        if (sum <= slider.maxValue)
+            slider.value += reput;
+    }
+}
